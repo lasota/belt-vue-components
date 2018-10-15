3a66c0657305f8cec37b47ffc334135bff6c6ad5
rob.lasota <rob.lasota@gmail.com>
2018-10-15 15:28:30 -0400

    making helpers available externally

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,25 @@
-import Vue from 'vue';
-import BeltClip from './BeltClip.vue';
+import Vue from 'vue'
+import BeltClip from './BeltClip.vue'
 
 const Components = {
     BeltClip
 }
 
 Object.keys(Components).forEach(name => {
-    Vue.component(name, Components[name]);
-});
+    Vue.component(name, Components[name])
+})
 
-export default Components;
+
+import CloudinaryDriver from '../helpers/ImageDrivers/CloudinaryDriver'
+import S3Driver from '../helpers/ImageDrivers/S3Driver'
+
+const Helpers = {
+    ImageDrivers: {
+        CloudinaryDriver,
+        S3Driver
+    }
+}
+
+export { Helpers }
+
+export default Components
